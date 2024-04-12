5e1a0fd0910463c157be2f3bafd38c4969c1ba57
intiqam <quluzade@intiqams-MacBook-Pro.local>
2024-04-12 19:52:37 +0400

    first-commit

---
+++ myFirstJavaProgram/src/main/java/myfirstjavaprogram/myfirstjavaprogram/MyFirstJavaProgram.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package myfirstjavaprogram.myfirstjavaprogram;
+
+/**
+ *
+ * @author quluzade
+ */
+public class MyFirstJavaProgram {
+
+    public static void main(String[] args) {
+//        int firstNumber = 55;
+//        int secondNumber = 66;
+//        double result = firstNumber / secondNumber;
+////        System.out.println(result);
+//
+////        char symbol = '?';
+//        System.out.println(symbol);
+//
+//        String name = "salam";
+////        System.out.println(name);
+//
+//        boolean in_stock = true;
+////        System.out.println(in_stock);
+
+        int a = 5;
+        ++a;
+        System.out.println(a);
+
+    }
+}
